27c4dd93ec42fcdf3a0da8b477a0a7eaefb2c4e5
lukman <mochammad.lukman@ralali.com>
2020-04-22 20:25:23 +0700

    change migration schema

--- migration/postgresql/20200422090432_users.up.sql
+++ migration/postgresql/20200422090432_users.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY,
   username varchar(100) DEFAULT NULL,
   email varchar(50) NOT NULL UNIQUE,
-  password varchar(100) NOT NULL UNIQUE,
-  PRIMARY KEY (id)
+  password varchar(100) NOT NULL UNIQUE
 );
--- migration/postgresql/20200422090910_user_balance_histories.up.sql
+++ migration/postgresql/20200422090910_user_balance_histories.up.sql
@@ -1,6 +1,6 @@
 CREATE TYPE type_transaction AS ENUM('credit', 'debit');
 CREATE TABLE IF NOT EXISTS user_balance_histories (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY,
   user_balance_id integer NOT NULL,
   balance_before integer NOT NULL,
   balance_after integer NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS user_balance_histories (
   type_activity type_transaction NOT NULL,
   ip varchar(20) NOT NULL,
   user_agent varchar(100) NOT NULL,
-  PRIMARY KEY (id)
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
